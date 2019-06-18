61125f3f4aa53c53f3d80c9bc3fc1da0843a6033
Yachareni, Krishnakanth <Krishnakanth.Yachareni@mastercard.com>
2019-06-18 18:37:55 +0530

    react fetching for news api

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -2,15 +2,41 @@ import React, { Component } from "react";
 import NewSingle from "./NewSingle";
 
 class News extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      news: []
+    };
+  }
+
+  componentDidMount() {
+    const url =
+      "https://newsapi.org/v2/top-headlines?apiKey=c56caeaa95b84e528efec9c5f71513b7&country=in&category=business";
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({
+          news: data.articles
+        });
+      })
+      .catch(error => console.log(error));
+  }
+
   renderItems() {
-    return this.props.items.map(item => {
-      <NewSingle key={item.id} item={item} />;
-    });
+    return this.state.news.map(item => (
+      <NewSingle key={item.url} item={item} />
+    ));
   }
 
   render() {
-    return <ul>{this.renderItems()}</ul>;
+    return (
+      <div className="row">
+        <ul>{this.renderItems()}</ul>
+      </div>
+    );
   }
 }
 
-export default News; 
+export default News;
